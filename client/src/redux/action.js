871b5f624273e846ac4bafefe89ce69e72d4ef7d
tinoyao <dicienbre013@gmail.com>
2022-09-15 17:46:51 -0300

    final commit

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -14,7 +14,7 @@ export const FILTER_BY_POPULATION = 'FILTER_BY_POPULATION';
 
 export function getCountries() {
     return async function (dispatch) {
-        const json = await axios.get('http://localhost:3001/countries');
+        const json = await axios.get('/countries');
         const data = json.data
         return dispatch({
             type: GET_COUNTRIES,
@@ -26,7 +26,7 @@ export function getCountries() {
 export function getCountryByName (name) {
     return async function (dispatch) {
         try {
-            const json = await axios.get('http://localhost:3001/countries?name=' + name)
+            const json = await axios.get('/countries?name=' + name)
             return dispatch ({
                 type: GET_BY_NAME,
                 payload: json.data
@@ -41,7 +41,7 @@ export function getCountryByName (name) {
 export function getDetailId (id) {
     return async function (dispatch) {
      try {
-         let json = await axios.get('http://localhost:3001/countries/' + id);
+         let json = await axios.get('/countries/' + id);
          return dispatch({
              type: GET_DETAILS,
              payload: json.data
@@ -66,7 +66,7 @@ export function resetCountries () {
 
 export function postActivity(payload){
     return async function(dispatch){
-        const json = await axios.post('http://localhost:3001/activities', payload)
+        const json = await axios.post('/activities', payload)
         return json
     }
 }
@@ -96,7 +96,7 @@ export function filterActivity(payload){
 
 export  function getActivities(){
     return async function(dispatch){
-        let json = await axios.get('http://localhost:3001/activities')
+        let json = await axios.get('/activities')
         const data = json.data
     return dispatch({
         type: GET_ACTIVITY,
